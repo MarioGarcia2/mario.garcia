1f75bfc46cb493345a208483cf68539905c9cb7d
MarioGarcia2 <43591278+MarioGarcia2@users.noreply.github.com>
2018-10-17 09:11:44 -0500

    Examen 2

---
+++ Examenipo/Examenipo/IVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examenipo
+{
+    interface IVehiculo
+    {
+     string Vehiculo { get; set; }
+        void Acelerar();
+        void Frenar();
+        void DarVueta();
+
+    }
+}
---
+++ Examenipo/Examenipo/Vehiculo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examenipo
+{
+   interface IVehiculo
+    {
+        
+    }
+    class Vehiculo : IVehiculo
+
+    {
+        string color = "", asientos = "", volante = "";
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public Vehiculo()
+        {
+
+        }
+
+        public Vehiculo(string color)
+        {
+            this.color = color;
+        }
+
+        public void Acelerar()
+        {
+            Console.WriteLine("El vheiculo esta avelerando");
+        }
+
+        public void Frenar()
+        {
+            Console.WriteLine("El Vehiculo esta frnando");
+        }
+
+        public void DarVueta()
+        {
+            Console.WriteLine("El Vehiculo esta dando Vuelta");
+        }
+    }
+}
